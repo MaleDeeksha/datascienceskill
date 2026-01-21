3eb820ce2eb938e9921794410eed872c15d11783
MaleDeeksha <103495775+MaleDeeksha@users.noreply.github.com>
2026-01-21 23:23:41 +0530

    Add files via upload

---
+++ python/Session-1.py
@@ -0,0 +1,10 @@
+# pip install --upgrade --quiet  langchain-google-genai
+from langchain_google_genai import GoogleGenerativeAI
+api_key="AIzaSyBUUejpbU_3eouCT96lkHmkE2mQ6jQb8TU"
+llm = GoogleGenerativeAI(model="gemini-pro", 
+                         google_api_key=api_key)
+print(
+    llm.invoke(
+        "List of top 5 Indain crickters"
+    )
+)
